3764f3f306380a7acae7ef7e6d0626f7def0c674
Arbaz2447 <127314616+Arbaz2447@users.noreply.github.com>
2025-08-09 21:31:55 +0530

    Update slidingWin-Dp.cpp

--- leetcodeC1/slidingWin-Dp.cpp
+++ leetcodeC1/slidingWin-Dp.cpp
@@ -2,17 +2,12 @@
 3634. Minimum Removals to Balance Array
 3635. Earliest Finish Time for Land and Water Rides II
 
-3638. Maximum Balanced Shipments(dp + stack)
-3640. Trionic Array II(hard dp)
 3639. Minimum Time to Activate String
+3640. Trionic Array II(hard dp) 
 
 // This Pattern ( New Found ) !
-209. Minimum Size Subarray Sum
-Sliding Window x3 Questions 
-
-
-
-
+209. Minimum Size Subarray Sum (pend ..!)
+904. Fruit Into Baskets (Sliding window ) (Daily Question)
 
 
 
@@ -49,7 +44,6 @@ public:
 class Solution {
 public:
     int minRemoval(vector<int>& nums, int k) {
-        
         // reverse the Q. Consider finding MAX balanced array ! 
         int left = 0;
         int n = nums.size();
@@ -73,7 +67,7 @@ public:
        return n - len;
     }
 };
-// Sliding Window Hard (Daily Q)
+// 2106. Maximum Fruits Harvested After at Most K Steps (Sliding Window Hard (Daily Q Day 2))
 class Solution {
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
@@ -228,7 +222,59 @@ public:
     }
 };
 
-
-
-
 */
+3640. Trionic Array II
+// Hard
+// You are given an integer array nums of length n.
+
+// A trionic subarray is a contiguous subarray nums[l...r] (with 0 <= l < r < n) for which there exist indices l < p < q < r such that:
+
+// nums[l...p] is strictly increasing,
+// nums[p...q] is strictly decreasing,
+// nums[q...r] is strictly increasing.
+// Return the maximum sum of any trionic subarray in nums.
+
+
+class Solution {
+public:
+    long long maxSumTrionic(vector<int>& a) {
+        int n = a.size();
+        /*
+                                  /dp[3]
+                       /\        /
+                dp[1] /  \      /
+        ------       /   dp[2]
+        dp[0]
+        dp[0] = start phase
+        dp[1] = 1st increaseing phase
+        dp[2] = 1st decreaseing phase
+        dp[3] = 2nd increasing
+        */
+        
+        vector<vector<long long>> dp(4, vector<long long>(n, -1e18));
+
+        long long ans = -1e18;
+        for(int i = 0; i < n; i++) {
+            dp[0][i] = a[i];
+            if(i > 0) {
+                //start increasing here OR we are alreading in increasing phase
+                if(a[i] > a[i - 1])
+                 dp[1][i] = max(dp[0][i - 1] + a[i], dp[1][i - 1] + a[i]);
+                //start decreasing phase here OR we are alreading in it
+                if(a[i] < a[i - 1])
+                 dp[2][i] = max(dp[1][i - 1] + a[i], dp[2][i - 1] + a[i]);
+                //start 2nd increasing phase or continue prev 2nd increasing phase
+                if(a[i] > a[i - 1])
+                 dp[3][i] = max(dp[2][i - 1] + a[i], dp[3][i - 1] + a[i]); 
+            }
+            ans = max(ans, dp[3][i]);
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
